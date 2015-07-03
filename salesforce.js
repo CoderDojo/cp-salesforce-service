68929ad9e7d38884616a8cd3d3a8a66069803a27
Damian Beresford <dberesford@gmail.com>
2015-07-03 19:44:24 +0100

    Added convert_lead_to_account

--- salesforce.js
+++ salesforce.js
@@ -1,12 +1,13 @@
 'use strict';
-
-module.exports = function (options) {
+module.exports = function (opts) {
   var seneca = this;
   var plugin = 'cd-salesforce';
 
   seneca.add({role: plugin, cmd: 'list_leads'}, cmd_list_leads);
   seneca.add({role: plugin, cmd: 'save_lead'}, cmd_save_lead);
   seneca.add({role: plugin, cmd: 'delete_lead'}, cmd_delete_lead);
+  seneca.add({role: plugin, cmd: 'convert_lead_to_account'}, cmd_convert_lead_to_account);
+
 
   function _userExistsInSalesForce(userId, cb) {
     var lead = seneca.make$('Lead');
@@ -42,6 +43,25 @@ module.exports = function (options) {
     var seneca = this;
     var lead = seneca.make$('Lead');
     lead.remove$(args.lead.Id, cb);
+  }
+
+  // We convert a Lead to an Account by calling a custom Apex endpoint.
+  // Note we do this directly in JSForce.
+  function cmd_convert_lead_to_account(args, cb) {
+    var jsforce = require('jsforce');
+    var conn = new jsforce.Connection(opts);
+
+    function _connect (cb) {
+      conn.identity(function (err, id) {
+        if (err) return conn.login(opts.username, opts.password, cb);
+        else return cb();
+      });
+    };
+
+    _connect(function (err) {
+      if (err) return cb(err);
+      conn.apex.get('/Lead/' + args.leadId, cb);
+    });
   }
 
   return {
