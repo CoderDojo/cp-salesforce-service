b2b8db271001deff5a222eb8d11a290a491d52db
Damian Beresford <dberesford@gmail.com>
2015-08-05 22:47:50 +0100

    Stop app before starting in start.sh

--- deploy/start-server.sh
+++ deploy/start-server.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+isExistApp=`ps -eaf |grep cp-salesforce-service |grep -v grep| awk '{ print $2; }'`
+if [[ -n $isExistApp ]]; then
+    service cp-salesforce-service stop
+fi
+
 service cp-salesforce-service start
