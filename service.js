94e1fdc1dd23cb17ba1bc312de4c68bf33f0ad2c
Damian Beresford <dberesford@gmail.com>
2015-08-11 17:41:19 +0100

    Seneca options fix

--- service.js
+++ service.js
@@ -3,7 +3,7 @@
 if (process.env.NEW_RELIC_ENABLED === "true") require('newrelic');
 
 var config = require('./config/config.js')();
-var seneca = require('seneca')();
+var seneca = require('seneca')(config);
 
 seneca.log.info('using config', JSON.stringify(config, null, 4));
 seneca.options(config);
