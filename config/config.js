c26967aafa1c180885912f0ff3819bd1d1ac3895
Damian Beresford <dberesford@gmail.com>
2015-09-23 09:51:37 +0200

    Removed dead seneca logging code, disabled actcache

--- config/config.js
+++ config/config.js
@@ -2,50 +2,6 @@ var assert = require('assert');
 var LogEntries = require('le_node');
 
 module.exports = function() {
-  function log () {
-    // seneca custom log handlers
-  
-    if (process.env.LOGENTRIES_ENABLED === 'true') {
-      assert.ok(process.env.LOGENTRIES_DEBUG_TOKEN, 'No LOGENTRIES_DEBUG_TOKEN set');
-      var led = new LogEntries({
-        token: process.env.LOGENTRIES_DEBUG_TOKEN,
-        flatten: true,
-        flattenArrays: true
-      });
-      
-      assert.ok(process.env.LOGENTRIES_ERRORS_TOKEN, 'No LOGENTRIES_ERROR_TOKEN set');
-      var lee = new LogEntries({
-        token: process.env.LOGENTRIES_ERRORS_TOKEN,
-        flatten: true,
-        flattenArrays: true
-      });
-    }
-  
-    function debugHandler() {
-      if (process.env.LOGENTRIES_ENABLED === 'true') {
-        assert.ok(process.env.LOGENTRIES_DEBUG_TOKEN, 'No LOGENTRIES_DEBUG_TOKEN set');
-        led.log('debug', arguments);
-      }
-    }
-  
-    function errorHandler() {
-      console.error(JSON.stringify(arguments));
-  
-      if (process.env.LOGENTRIES_ENABLED === 'true') {
-        assert.ok(process.env.LOGENTRIES_ERRORS_TOKEN, 'No LOGENTRIES_ERROR_TOKEN set');
-        lee.log('err', arguments);
-      }
-    }
-  
-    return {
-      map:[{
-        level:'debug', handler: debugHandler
-      }, {
-        level:'error', handler: errorHandler
-      }]
-    };
-  };
-
   return {
     transport: {
       type: 'web',
@@ -59,7 +15,7 @@ module.exports = function() {
       username: process.env.SALESFORCE_USERNAME,
       password: process.env.SALESFORCE_PASSWORD
     },
-    strict: {add:false,  result:false},
-    log: log()
+    strict: {add: false,  result: false},
+    actcache: {active: false}
   };
 }
